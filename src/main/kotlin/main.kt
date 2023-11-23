35c05e8718020f2e22ddcf3d01073121b4ad9c8d
YS <etgt777@naver.com>
2023-11-23 11:13:37 +0900

    code kata 58

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,37 +1,38 @@
-import java.math.
-
 class Solution {
-    fun solution(answers: IntArray): IntArray {
-        var answer = intArrayOf()
+    fun solution(nums: IntArray): Int {
+        var answer = 0
 
-        var point = intArrayOf(0, 0, 0)
+        for(i in 0 until nums.size - 2) {
+            for (j in i + 1 until nums.size -1){
+                for (k in j+1 until nums.size){
+                    var combi = nums[i] + nums [j] + nums[k]
 
-        for (i in 0 .. answers.lastIndex) {
-            val one = intArrayOf(1, 2, 3, 4, 5)
-            val two = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
-            val three = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
-
-            if(answers[i] == one[i % answers.size]) {point.set(0, point[0] + 1)}
-            if(answers[i] == two[i % answers.size]) {point.set(1, point[1] + 1)}
-            if(answers[i] == three[i % answers.size]) {point.set(2, point[2] + 1)}
-        }
-
-        for (i in 0 .. point.lastIndex) {
-            if (point[i] == point.maxOrNull()) {
-                answer += i + 1
+                    if(check(combi)){
+                        answer += 1
+                    }
+                }
             }
         }
 
-        answer.sorted()
-
-
         return answer
+
     }
 }
 
+fun check (n: Int): Boolean{
+    for (c in 2 until n) {
+        if (n % c == 0) {
+            return false
+        }
+    }
+    return true
+}
 fun main() {
     var n = Solution()
 
-    println(n.solution(intArrayOf(1,2,3,4,5)).joinToString())
-    println(n.solution(intArrayOf(1,3,2,4,2)).joinToString())
+    println(n.solution(intArrayOf(1, 2, 3, 4)))
+    println(n.solution(intArrayOf(1, 2, 7, 6, 4)))
+    println(n.solution(intArrayOf(1, 2, 3, 9, 10, 7, 6, 4)))
+
 }
+
